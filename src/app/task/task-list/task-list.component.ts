f754b6682dde248ff95dbf6c316757a6f2944589
toru aoki <totolog1126@gmail.com>
2022-01-23 17:02:28 +0900

    add: function of extend, mouseenter, mouseleave, *ngIf

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/model/task';
 import { TaskService } from 'src/app/services/task.service';
 
+class TaskListElement extends Task {
+  hovered: boolean = false;
+}
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
-  tasks: Task[] = [];
+  tasks: TaskListElement[] = [];
 
   constructor(
     private taskService: TaskService,
@@ -16,8 +20,21 @@ export class TaskListComponent implements OnInit {
 
   ngOnInit(): void {
     this.taskService.list().subscribe((tasks: Task[]) => {
-      this.tasks = tasks;
+      this.tasks = tasks.map((task: Task) => {
+        return {
+          ...task,
+          hovered: false,
+        }
+      })
     })
   }
+
+  hovered(task: TaskListElement): void {
+    task.hovered = true
+  };
+
+  unhovered(task: TaskListElement): void {
+    task.hovered = false
+  };
 
 }
